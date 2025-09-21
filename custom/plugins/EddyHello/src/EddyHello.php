1e0c31d11492a01e2645b7b98647bd5ec221b202
Edgar Lima Santos Rodrigues <limae481@gmail.com>
2025-09-21 21:39:23 +0200

    Test plugin zum lernen

---
+++ custom/plugins/EddyHello/src/EddyHello.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace Eddy\EddyHello;
+
+use Shopware\Core\Framework\Plugin;
+
+class EddyHello extends Plugin
+{
+    // Noch leer. Reicht völlig aus, damit Shopware das Plugin laden kann.
+}
